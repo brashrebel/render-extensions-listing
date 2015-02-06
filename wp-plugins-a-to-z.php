a5650a9b44725bc3dbe3e46e02d38dc8d97adcd5
Kyle Maurer <kyle@realbigmarketing.com>
2015-02-06 10:43:44 -0500

    Moved object assignment into a new method. Began assigning some values from source. Unable to get post meta currently.

--- wp-plugins-a-to-z.php
+++ wp-plugins-a-to-z.php
@@ -65,62 +65,74 @@ class WP_Plugins_AtoZ {
 		}
 	}
 
+	public function setup_results() {
+		// Building an object which matches what the plugins list table expects
+		$objects = array();
+
+		foreach ( $this->get_stuff() as $plugin ) {
+			$object                    = new stdClass();
+			$object->name              = $plugin->title;
+			$object->slug              = $plugin->slug;
+			$object->version           = '1.0';
+			$object->author            = 'Here we go';
+			$object->author_profile    = 'Here we go';
+			$object->contributors      = array( 'Kyle' => 'http://kyleblog.net' );
+			$object->requires          = '1.0';
+			$object->tested            = '4.0';
+			$object->compatibility     = array( '4.0' => array( '1.0' => array( '100' ) ) );
+			$object->rating            = 99.5;
+			$object->num_ratings       = 234;
+			$object->ratings           = array(
+				'5' => 25,
+				'4' => 2,
+				'3' => 1,
+				'2' => 8,
+				'1' => 1
+			);
+			$object->description       = 'Here we go';
+			$object->short_description = 'Here we go';
+			$object->icons             = array( 'default' => '#' );
+			$object->downloaded        = 1100;
+			$object->last_updated      = '1 month ago';
+
+			$objects[] = $object;
+		}
+
+		return $objects;
+	}
+
 	/**
 	 * Contents of the tab
 	 */
 	public function contents() {
 		global $wp_list_table;
-//		$data = $this->get_stuff();
-//		echo 'DEBUGGING<pre>';
-//		print_r( $data );
+
+//		echo 'TESTING<pre>';
+//		var_dump( $this->get_stuff() );
 //		echo '</pre>';
-
-		$object = new stdClass();
-		$object->name = 'Here we go';
-		$object->slug = 'Here we go';
-		$object->version = '1.0';
-		$object->author = 'Here we go';
-		$object->author_profile = 'Here we go';
-		$object->contributors = array( 'Kyle' => 'http://kyleblog.net' );
-		$object->requires = '1.0';
-		$object->tested = '4.0';
-		$object->compatibility = array( '4.0' => array( '1.0' => array( '100' )));
-		$object->rating = 99.5;
-		$object->num_ratings = 234;
-		$object->ratings = array(
-			'5' => 25,
-			'4' => 2,
-			'3' => 1,
-			'2' => 8,
-			'1' => 1
-		);
-		$object->description = 'Here we go';
-		$object->short_description = 'Here we go';
-		$object->icons = array('default' => '#');
-		$object->downloaded = 1100;
-		$object->last_updated = '1 month ago';
-
-		$object = array( $object );
+		// Testing the contents of our manually built object
 		echo 'TESTING<pre>';
-		var_dump($object);
+		var_dump( $this->setup_results() );
 		echo '</pre>';
 
+		// Used for customizing a .org call
 		$args = array( 'user' => 'BrashRebel' );
+		$api  = plugins_api( 'query_plugins', $args );
 
-		$api = plugins_api( 'query_plugins', $args );
+		// Give the table list the source array of objects
+		$wp_list_table->items = $this->setup_results();//$api->plugins;
 
-		$wp_list_table->items = $object;//$api->plugins;
-
+		// Testing the output from our .org call
 		echo 'DEBUGGING<pre>';
-		var_dump($api->plugins);
+		var_dump( $api->plugins );
 		echo '</pre>';
 
-		$wp_list_table->set_pagination_args(
-			array(
-				'total_items' => $api->info['results'],
-				'per_page'    => 5,
-			)
-		);
+//		$wp_list_table->set_pagination_args(
+//			array(
+//				'total_items' => $api->info['results'],
+//				'per_page'    => 5,
+//			)
+//		);
 	}
 }
 
